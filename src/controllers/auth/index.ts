75bf610210bac36ddef00aeeab9be652bf3ca2bd
Misha Kravtsov <aniahucm@gmail.com>
2022-09-29 20:08:55 +0300

    working on auth services

--- src/controllers/auth/index.ts
+++ src/controllers/auth/index.ts
@@ -6,11 +6,13 @@ import jwt from 'jsonwebtoken';
 import { JWT_ACCES_SECRET_KEY } from '@/env';
 import Roles from '@/constants/db/models/user/roles';
 import AuthService from '@/services/auth';
+import CookiesStorageKeys from '@/constants/controllers/auth/cookiesStorageKeys';
 
 class AuthController {
   async registration(req: Request, res: Response, next: NextFunction) {
     const { data } = req.body as { data: LoginCredentials };
     const { login, password } = data;
+
     try {
       await AuthService.registration(login, password, Roles.USER);
       return res.status(200).json('Ok');
@@ -20,22 +22,15 @@ class AuthController {
   }
 
   async login(req: Request, res: Response, next: NextFunction) {
-    try {
-      const { data } = req.body as { data: LoginCredentials };
-      const { login, password } = data;
-      const user = await User.findOne({ login });
-      if (!user) {
-        return res.status(404).json({ error: 'User not found' });
-      }
-      const isPWDValid = bcrypt.compareSync(password, user.password);
-      if (!isPWDValid) {
-        return res.status(400).json({ error: 'Invalid password' });
-      }
-      const isAdmin = user.role === Roles.ADMIN;
-      const accesToken = jwt.sign({ id: user._id, isAdmin }, JWT_ACCES_SECRET_KEY, { expiresIn: '1h' });
-      const refreshToken = jwt.sign({ id: user._id, isAdmin }, JWT_ACCES_SECRET_KEY, { expiresIn: '30d' });
+    const { data } = req.body as { data: LoginCredentials };
+    const { login, password } = data;
 
-      // const refreshTokenDocument = new RefreshToken();
+    try {
+      const { accesToken, refreshToken } = await AuthService.login(login, password);
+      res.cookie(CookiesStorageKeys.REFRESH_TOKEN, refreshToken, {
+        secure: false,
+        httpOnly: true,
+      });
       return res.json({ accesToken, refreshToken });
     } catch (error) {
       res.status(500).json({ error: 'Server error' });
--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,6 +1,11 @@
 import Roles from '@/constants/db/models/user/roles';
 import User from '@/db/models/user/model';
+import { JWT_ACCES_SECRET_KEY } from '@/env';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import RefreshToken from '@/db/models/refreshToken/model';
+import { resolve } from 'path';
+import { Response } from 'express';
 
 class AuthService {
   async registration(login: string, password: string, role: Roles) {
@@ -19,6 +24,28 @@ class AuthService {
     });
 
     await userCandidate.save();
+  }
+
+  async login(login: string, password: string) {
+    const user = await User.findOne({ login });
+
+    if (!user) {
+      throw new Error(`User with login: ${login} does not exist`);
+    }
+    const isPasswordValid = bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new Error(`Incorrect password`);
+    }
+
+    const isAdmin = user.role === Roles.ADMIN;
+    const accesToken = jwt.sign({ id: user._id, isAdmin }, JWT_ACCES_SECRET_KEY, { expiresIn: '1h' });
+    const refreshToken = jwt.sign({ id: user._id, isAdmin }, JWT_ACCES_SECRET_KEY, { expiresIn: '30d' });
+
+    const refreshTokenDocument = new RefreshToken({ user: user._id, refreshToken });
+
+    await refreshTokenDocument.save();
+
+    return { accesToken, refreshToken };
   }
 }
 
