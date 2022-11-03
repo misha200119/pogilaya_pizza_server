49381b8f20129081e21c7aef502f379b03d21160
Misha Kravtsov <aniahucm@gmail.com>
2022-11-03 13:36:14 +0200

    fixed multible domains option delegate cors function

--- src/constants/middlewares/cors/index.ts
+++ src/constants/middlewares/cors/index.ts
@@ -1,7 +1,17 @@
 import cors from 'cors';
 import { CLIENT_URL, CLIENT_PORT, isProd, CORSAllowedURLs } from '@/env';
 
-export default cors({
-  credentials: true,
-  origin: [isProd ? CLIENT_URL : `${CLIENT_URL}:${CLIENT_PORT}`, ...CORSAllowedURLs],
-});
+const allowedURLs = [isProd ? CLIENT_URL : `${CLIENT_URL}:${CLIENT_PORT}`, ...CORSAllowedURLs];
+
+const corsOptionsDelegate = (req, callback) => {
+  let corsOptions;
+
+  if (allowedURLs.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { credentials: true, origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { credentials: true, origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions);
+};
+
+export default cors(corsOptionsDelegate);
