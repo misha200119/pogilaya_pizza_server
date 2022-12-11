622f5192fd3001ef54e4739dcfbefac5650fae25
Misha Kravtsov <aniahucm@gmail.com>
2022-12-11 14:03:07 +0200

    minor fixes

--- src/constants/middlewares/cors/index.ts
+++ src/constants/middlewares/cors/index.ts
@@ -9,7 +9,7 @@ const corsOptionsDelegate = (req, callback) => {
   if (allowedURLs.indexOf(req.header('Origin')) !== -1) {
     corsOptions = { credentials: true, origin: true }; // reflect (enable) the requested origin in the CORS response
   } else {
-    corsOptions = { credentials: true, origin: true }; // disable CORS for this request
+    corsOptions = { credentials: true, origin: false }; // disable CORS for this request
   }
   callback(null, corsOptions);
 };
